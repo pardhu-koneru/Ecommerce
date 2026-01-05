4edec4c298bca35ec6ec169433a92a774a28703c
Pardha Saradhi Koneru <pardhukoneru25@gmail.com>
2026-01-05 15:12:52 +0530

    Email verification implemented

---
+++ ecommerce/users/managers.py
@@ -0,0 +1,18 @@
+from django.contrib.auth.base_user import BaseUserManager
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None):
+        if not email:
+            raise ValueError("Email is required")
+
+        user = self.model(email=self.normalize_email(email))
+        user.set_password(password)  # 🔐 hashing
+        user.save()
+        return user
+
+    def create_superuser(self, email, password):
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save()
+        return user
